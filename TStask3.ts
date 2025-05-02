7f70370d27919cc6a7d62296d1cb47eca756ee5c
Eugena <eugena.h12345@gmail.com>
2025-05-02 23:51:48 +0300

    add const getData

---
+++ TStask3.ts
@@ -0,0 +1,28 @@
+const COMMENTS_URL = "https://jsonplaceholder.typicode.com/comments";
+
+interface Post {
+  postId: number;
+  id: number;
+  name: string;
+  email: string;
+  body: string;
+}
+
+const getData = async (url: string): Promise<Post[]> => {
+  const response = await fetch(url);
+  if (response.ok) {
+    return response.json();
+  }
+  throw new Error("Something went wrong");
+};
+
+getData(COMMENTS_URL)
+  .then((data) => {
+    console.log(data);
+    data.forEach((post) => {
+      console.log(`ID: ${post.id}, Email: ${post.email.split("@")[0]}...`);
+    });
+  })
+  .catch((e) => {
+    console.log(e);
+  });
